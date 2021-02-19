5cdc19da7f6d0dc7e97dddfd8e6277e8bdf9257c
James Nair <vanairngo2@hotmail.com>
2021-02-19 20:53:38 +1000

    add style to ChatInput

--- src/components/message/ChatInput.tsx
+++ src/components/message/ChatInput.tsx
@@ -1,11 +1,28 @@
-import { Button, TextField } from "@material-ui/core"
+import { Button, IconButton, InputBase, makeStyles } from "@material-ui/core"
 import React, { ChangeEvent, FormEvent, useState } from "react"
+import SendIcon from '@material-ui/icons/Send';
+const useStyles = makeStyles( () => ({
+    container:{
+        width: "auto",
+        height: "auto"
+    },
+
+    textBox: {
+        width: "85%",
+        marginLeft: "5px",
+        fontSize: "20px"
+    },
+    button:{
+        padding: "10"
+    }
+}))
 
 type ChatInputPropsType = {
     send: Function
 }
 
 export const ChatInput = (props: ChatInputPropsType) => {
+    const classes = useStyles();
     const [message, setMessage] = useState("");
 
     const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement> ) => {
@@ -18,8 +35,11 @@ export const ChatInput = (props: ChatInputPropsType) => {
         setMessage("")
     }
     
-    return <form noValidate onSubmit={handleSubmit}>
-        <TextField label="Start typing ...." variant="outlined" onChange={handleTextChange} value={message}/>
-        <Button variant="contained" type="submit" >Send</Button>
+    return <form className={classes.container} noValidate onSubmit={handleSubmit} >
+        
+        <InputBase className={classes.textBox} placeholder="Start typing..." inputProps={{"aria-label": "start typing ..."}} onChange={handleTextChange} value={message} />
+        <IconButton className={classes.button} color="primary" aria-label="send">
+            <SendIcon />
+        </IconButton>
     </form>
 }
