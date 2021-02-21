7e71664e8483ee27b0b941519fc09657f8c99d1e
James Nair <vanairngo2@hotmail.com>
2021-02-21 21:10:49 +1000

    include name in message

--- src/components/message/ChatWindow.tsx
+++ src/components/message/ChatWindow.tsx
@@ -1,24 +1,27 @@
-import { makeStyles } from "@material-ui/styles"
+import { makeStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles( () => ({
-    window: {
-        height: "80%",
-        minHeight: "300px",
-        border: "1px solid grey"
-    }
-}))
+const useStyles = makeStyles(() => ({
+  window: {
+    height: "80%",
+    minHeight: "300px",
+    border: "1px solid grey",
+  },
+}));
 
 type ChatWindowPropsType = {
-    messages: Array<string>
-}
+  name: string;
+  messages: Array<string>;
+};
 
 export const ChatWindow = (props: ChatWindowPropsType) => {
-    const classes = useStyles();
-    return <div className={classes.window}>
-        <ul>
-        {props.messages.map( eachMessage => <li>{eachMessage}</li> )}
-
-        </ul>
-
+  const classes = useStyles();
+  return (
+    <div className={classes.window}>
+      {props.messages.map((eachMessage: string, index: number) => (
+        <div key={"message_" + index}>
+          {props.name} : <span>{eachMessage}</span>
+        </div>
+      ))}
     </div>
-}
+  );
+};
--- src/views/Message.tsx
+++ src/views/Message.tsx
@@ -1,36 +1,55 @@
-import { makeStyles } from "@material-ui/core"
-import React, { useState } from "react"
-import { ChatInput } from "../components/message/ChatInput"
-import { ChatWindow } from "../components/message/ChatWindow"
+import { InputBase, makeStyles } from "@material-ui/core";
+import React, { useState } from "react";
+import { ChatInput } from "../components/message/ChatInput";
+import { ChatWindow } from "../components/message/ChatWindow";
 
-const useStyles = makeStyles( () => ({
-    root: {
-       width: "100%",
-       height: "100%"
-    },
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "100%",
+    height: "100%",
+  },
 
-    container: {
-        width: "100%",
-        maxWidth: "480px",
-        margin: "10% auto",
-        border: "1px solid black"
-    }
-}))
+  container: {
+    width: "100%",
+    maxWidth: "480px",
+    margin: "10% auto",
+    border: "1px solid black",
+  },
+
+  nameBox: {
+    width: "85%",
+    marginLeft: "5px",
+    fontSize: "20px",
+  },
+}));
 export const Message = () => {
-    const classes = useStyles();
-    const [chatLog, setChatLog] = useState<Array<string>>([])
+  const classes = useStyles();
+  const [chatLog, setChatLog] = useState<Array<string>>([]);
+  const [name, setName] = useState("");
 
-    const addToChatLog = (message: string) => {
-        let currentChatLog = [...chatLog];
-        currentChatLog.push(message);
-        setChatLog(currentChatLog);
-    }
-    
-    return <div className={classes.root}>
-        <div className={classes.container}>
-        <ChatWindow messages={chatLog}/>
+  const addToChatLog = (message: string) => {
+    let currentChatLog = [...chatLog];
+    currentChatLog.push(message);
+    setChatLog(currentChatLog);
+  };
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setName(event.currentTarget.value);
+  };
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <InputBase
+          className={classes.nameBox}
+          placeholder="My name is ..."
+          inputProps={{ "aria-label": "My name is ..." }}
+          onChange={handleTextChange}
+          value={name}
+        />
+        <ChatWindow name={name} messages={chatLog} />
         <ChatInput send={addToChatLog} />
-
-        </div>
+      </div>
     </div>
-}
+  );
+};
